69bdcd089de489f9444f068e9d8033a9ad01855c
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-07 16:27:25 -0800

    Created Logo comp and added to signin/signup pages

---
+++ components/Logo.tsx
@@ -0,0 +1,16 @@
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+	return (
+		<Link href="/" className="flex items-center gap-2">
+			<PiggyBank className="stroke stroke-amber-500 stroke-[1.5] h-11 w-11" />
+			<p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold text-transparent leading-right tracking-tighter">
+				BudgetTracker
+			</p>
+		</Link>
+	);
+}
+
+export default Logo;
