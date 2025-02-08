aedecc0832080e76d6c3adeaec56eef5ce5e1823
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-07 17:15:19 -0800

    Set up desktop and mobile navbars

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
-function Layout() {
-	return <div>Layout</div>;
+function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="relative flex h-screen w-full flex-col">
+			<Navbar />
+			<div className="w-full">{children}</div>
+		</div>
+	);
 }
 
 export default Layout;
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -12,5 +12,14 @@ function Logo() {
 		</Link>
 	);
 }
+export function LogoMobile() {
+	return (
+		<Link href="/" className="flex items-center gap-2">
+			<p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold text-transparent leading-right tracking-tighter">
+				BudgetTracker
+			</p>
+		</Link>
+	);
+}
 
 export default Logo;
