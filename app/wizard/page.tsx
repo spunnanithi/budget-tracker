9527c62a4052e8126184a36c91aa6e78955572e5
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-08 20:15:02 -0800

    Set up card and button structure

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,3 +1,5 @@
+import Logo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
@@ -7,6 +9,7 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -31,6 +34,22 @@ async function Page() {
 				</h3>
 			</div>
 			<Separator />
+			<Card className="w-full">
+				<CardHeader>
+					<CardTitle>Currency</CardTitle>
+					<CardDescription>
+						Set your default currency for transactions
+					</CardDescription>
+				</CardHeader>
+				<CardContent></CardContent>
+			</Card>
+			<Separator />
+			<Button className="w-full" asChild>
+				<Link href={"/"}>I&apos;m done! Take me to the dashboard!</Link>
+			</Button>
+			<div className="mt-8">
+				<Logo />
+			</div>
 		</div>
 	);
 }
