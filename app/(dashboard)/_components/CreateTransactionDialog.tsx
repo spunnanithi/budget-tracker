90daa36037148a9f649af4fc9e1bc2ed1a83246b
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-11 13:12:04 -0800

    Created create transaction server actions

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -2,7 +2,9 @@
 
 import {
 	Dialog,
+	DialogClose,
 	DialogContent,
+	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
@@ -24,9 +26,19 @@ import {
 	FormField,
 	FormItem,
 	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import CategoryPicker from "./CategoryPicker";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { Calendar1Icon, Loader2 } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
 
 interface Props {
 	trigger: ReactNode;
@@ -121,9 +133,66 @@ function CreateTransactionDialog({ trigger, type }: Props) {
 									</FormItem>
 								)}
 							/>
+
+							{/* Form  Date*/}
+							<FormField
+								control={form.control}
+								name="date"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Transaction Date</FormLabel>
+										<Popover>
+											<PopoverTrigger asChild>
+												<FormControl>
+													<Button
+														variant={"outline"}
+														className={cn(
+															"w-[200px] pl-3 text-left font-normal",
+															!field.value && "text-muted-foreground"
+														)}>
+														{field.value ? (
+															format(field.value, "PPP")
+														) : (
+															<span>Pick a date</span>
+														)}
+														<Calendar1Icon className="ml-auto h-4 w-4 opacity-50" />
+													</Button>
+												</FormControl>
+											</PopoverTrigger>
+											<PopoverContent className="w-auto p-0">
+												<Calendar
+													mode="single"
+													selected={field.value}
+													onSelect={field.onChange}
+													initialFocus
+												/>
+											</PopoverContent>
+										</Popover>
+										<FormDescription>
+											Select a date for this transaction
+										</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 						</div>
 					</form>
 				</Form>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button
+							variant={"secondary"}
+							type="button"
+							onClick={() => {
+								form.reset();
+							}}>
+							Cancel
+						</Button>
+					</DialogClose>
+					<Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+						{isPending ? <Loader2 className="animate-spin" /> : "Create"}
+					</Button>
+				</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
