c3187f5e8608269d923daf3446aee3247101f01a
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-10 17:03:21 -0800

    Set up dialog components for income and expense buttons

---
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface Props {
+	trigger: ReactNode;
+	type: TransactionType;
+}
+
+import React from "react";
+
+function CreateTransactionDialog({ trigger, type }: Props) {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>{trigger}</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>
+						Create a new{" "}
+						<span
+							className={
+								(cn("m-1"),
+								type === "income" ? "text-emerald-500" : "text-red-500")
+							}>
+							{type}
+						</span>{" "}
+						transation
+					</DialogTitle>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+export default CreateTransactionDialog;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 
 async function Page() {
 	const user = await currentUser();
@@ -26,18 +27,25 @@ async function Page() {
 			<div className="border-b bg-card">
 				<div className="container flex flex-wrap items-center justify-between gap-6 py-8">
 					<p className="text-3xl font-bold">Hello, {user.firstName}!</p>
-
 					<div className="flex items-center gap-3">
-						<Button
-							variant={"outline"}
-							className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white">
-							New income
-						</Button>
-						<Button
-							variant={"outline"}
-							className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white">
-							New expense
-						</Button>
+						<CreateTransactionDialog
+							trigger={
+								<Button
+									variant={"outline"}
+									className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white">
+									New income
+								</Button>
+							}
+							type="income"></CreateTransactionDialog>
+						<CreateTransactionDialog
+							trigger={
+								<Button
+									variant={"outline"}
+									className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white">
+									New expense
+								</Button>
+							}
+							type="expense"></CreateTransactionDialog>
 					</div>
 				</div>
 			</div>
