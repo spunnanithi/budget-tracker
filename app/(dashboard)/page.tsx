3a4d96c69aadb8a348c88ae08a6531552640604f
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-10 16:54:26 -0800

    Created header for dashboard with income and expense buttons

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function Page() {
-	return <div>Page</div>;
+async function Page() {
+	const user = await currentUser();
+
+	if (!user) {
+		redirect("/sign-in");
+	}
+
+	const userSettings = await prisma.userSettings.findUnique({
+		where: {
+			userId: user.id,
+		},
+	});
+
+	if (!userSettings) {
+		redirect("/wizard");
+	}
+
+	return (
+		<div className="h-full bg-background">
+			<div className="border-b bg-card">
+				<div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+					<p className="text-3xl font-bold">Hello, {user.firstName}!</p>
+
+					<div className="flex items-center gap-3">
+						<Button
+							variant={"outline"}
+							className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white">
+							New income
+						</Button>
+						<Button
+							variant={"outline"}
+							className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white">
+							New expense
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Page;
