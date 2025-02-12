89d71d15456cb37fd136c00b88772fc53ddcb4f5
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-11 17:45:39 -0800

    Implemented server actions and UI for income, expense, and balance stat cards

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="relative flex h-screen w-full flex-col">
 			<Navbar />
-			<div className="w-full">{children}</div>
+			<div className="w-full mx-4">{children}</div>
 		</div>
 	);
 }
