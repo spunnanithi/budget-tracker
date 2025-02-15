c3b51492f5a39866a2b97699bc9a2d8e0e03dab2
Sirasit Punnanithi <dev@energyaisolutions.com>
2025-02-14 16:41:23 -0800

    Implemented month selector for history

--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -53,6 +53,14 @@ function HistoryPeriodSelector({
 						years={historyPeriods.data || []}
 					/>
 				</SkeletalWrapper>
+
+				{timeFrame === "month" && (
+					<SkeletalWrapper
+						isLoading={historyPeriods.isFetching}
+						fullWidth={false}>
+						<MonthSelector period={period} setPeriod={setPeriod} />
+					</SkeletalWrapper>
+				)}
 			</div>
 		</div>
 	);
@@ -84,6 +92,40 @@ function YearSelector({
 						{year}
 					</SelectItem>
 				))}
+			</SelectContent>
+		</Select>
+	);
+}
+
+function MonthSelector({
+	period,
+	setPeriod,
+}: {
+	period: Period;
+	setPeriod: (period: Period) => void;
+}) {
+	return (
+		<Select
+			value={period.month.toString()}
+			onValueChange={(value) => {
+				setPeriod({ year: period.year, month: parseInt(value) });
+			}}>
+			<SelectTrigger className="w-[180px]">
+				<SelectValue />
+			</SelectTrigger>
+			<SelectContent>
+				{[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((month) => {
+					const monthStr = new Date(period.year, month, 1).toLocaleString(
+						"default",
+						{ month: "long" }
+					);
+
+					return (
+						<SelectItem key={month} value={month.toString()}>
+							{monthStr}
+						</SelectItem>
+					);
+				})}
 			</SelectContent>
 		</Select>
 	);
